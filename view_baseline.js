87b5974082657e3b872e256cb40985d292a663c0
Temi <i.temi@wustl.edu>
2024-03-31 20:17:32 -0500

    changed file name to file path

--- view_baseline.js
+++ view_baseline.js
@@ -31,7 +31,7 @@ perm_dialog = define_new_dialog('permdialog', title='Permissions', options = {
 // Make the initial "Object Name:" text:
 // If you pass in valid HTML to $(), it will *create* elements instead of selecting them. (You still have to append them, though)
 //(Temi)Simplify language in dialog box
-obj_name_div = $('<div id="permdialog_objname" class="section"> File Name: <span id="permdialog_objname_namespan"></span> </div>')
+obj_name_div = $('<div id="permdialog_objname" class="section"> File Path: <span id="permdialog_objname_namespan"></span> </div>')
 
 //Make the div with the explanation about special permissions/advanced settings:
 advanced_expl_div = $('<div id="permdialog_advanced_explantion_text">For special permissions or advanced settings, click Advanced.</div>')
