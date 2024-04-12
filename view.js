92c22865f3aafb204ea229c12bd7f544293d2184
khuuushi <70972540+khuuushi@users.noreply.github.com>
2024-04-11 23:58:05 -0500

    updating dialog

--- view.js
+++ view.js
@@ -57,11 +57,15 @@
         let currentType = $(this).attr('ptype'); 
         let oppositeType = currentType === 'allow' ? 'deny' : 'allow';
         let oppositeCheckbox = $(`input[group="${currentGroup}"][ptype="${oppositeType}"]`);
-        console.log(checked)
-        console.log(currentGroup)
-        console.log(currentType)
-        console.log(oppositeType)
-        console.log(oppositeCheckbox)
+        if(checked.is(':checked')) {
+            confirmation_dialog.empty().append(currentGroup + " is now set to " + currentType).dialog('open')
+        }
+        confirmation_dialog.empty().append("You removed the " + currentType + " permission from " + currentGroup).dialog('open')
+        // console.log(checked)
+        // console.log(currentGroup)
+        // console.log(currentType)
+        // console.log(oppositeType)
+        // console.log(oppositeCheckbox)
     })
 
 //(Brian) this is the actual function that toggles the permissions. It should be connected to the backend. LMK if it needs to be modified.
