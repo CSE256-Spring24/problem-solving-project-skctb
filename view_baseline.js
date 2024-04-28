68df55fb4aaf2ba096c5ce42b076ae660a122e4e
khuuushi <70972540+khuuushi@users.noreply.github.com>
2024-04-28 17:50:55 -0500

    text

--- view_baseline.js
+++ view_baseline.js
@@ -151,11 +151,11 @@ perm_dialog.append(obj_name_div)
 perm_dialog.append($('<div id="permissions_user_title">Current group or user names with permission constraint for this file:</div><br>'))
 //(Temi) Appended information about advanced settings
 perm_dialog.append($('<div id="permissions_user_title">If a specific user  isnt listed below but, their group is, check advanced settings under edit permissions</div>'))
+perm_dialog.append("Check First: If a group has permissions set to them by a parent, make sure each file allows parent permission. In order to check, go to Advanced Permissions & ensure the box allowing parent permissions is checked.")
 perm_dialog.append(file_permission_users)
 perm_dialog.append(perm_add_user_select)
 perm_add_user_select.append(perm_remove_user_button) // Cheating a bit again - add the remove button the the 'add user select' div, just so it shows up on the same line.
 perm_dialog.append(grouped_permissions)
-perm_dialog.append("Check First: If a group has permissions set to them by a parent, make sure each file allows parent permission. In order to check, go to Advanced Permissions & ensure the box allowing parent permissions is checked.")
 perm_dialog.append("Note: If a group is denied a permission, the all members of that group will be denied access. Removing a 'Deny' constraint will not give the group access unless a member is given an 'Allow'.")
 perm_dialog.append(advanced_expl_div)
 
