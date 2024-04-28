0c1e70815978c1e2a2dee623ee03c388f92ee770
khuuushi <70972540+khuuushi@users.noreply.github.com>
2024-04-28 17:52:48 -0500

    text

--- view_baseline.js
+++ view_baseline.js
@@ -152,7 +152,7 @@ perm_dialog.append($('<div id="permissions_user_title">Current group or user nam
 //(Temi) Appended information about advanced settings
 perm_dialog.append($('<div id="permissions_user_title">If a specific user  isnt listed below but, their group is, check advanced settings under edit permissions</div>'))
 perm_dialog.append("")
-perm_dialog.append("Check First: If a group has permissions set to them by a parent, make sure each file allows parent permission. In order to check, go to Advanced Permissions & ensure the box allowing parent permissions is checked.")
+perm_dialog.append("<strong>Check First: If a group has permissions set to them by a parent, make sure each file allows parent permission. In order to check, go to Advanced Permissions & ensure the box allowing parent permissions is checked.")
 perm_dialog.append(file_permission_users)
 perm_dialog.append(perm_add_user_select)
 perm_add_user_select.append(perm_remove_user_button) // Cheating a bit again - add the remove button the the 'add user select' div, just so it shows up on the same line.
